72d44a53064bafe6ed866f7e0e0936a5418f3cba
Luan Tran <noblakit01@gmail.com>
2019-04-16 16:29:00 +0700

    Add template file for custom simple view from code

---
+++ LX Simple View from code.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpViews() {
+        
+    }
+    
+}
