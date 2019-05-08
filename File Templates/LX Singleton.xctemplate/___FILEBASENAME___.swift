5cb172cd93f4dbd4ac1a471424eaae869f64de37
Luan Tran <noblakit01@gmail.com>
2019-05-08 16:38:13 +0700

    Add LX Singleton template

---
+++ File Templates/LX Singleton.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+    
+    public static let shared = ___FILEBASENAMEASIDENTIFIER___()
+    
+    private init() {
+    }
+    
+}
